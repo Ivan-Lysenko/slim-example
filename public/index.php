0a4c20adc31444a6d2b9857c3eca2cde7e278c7a
Ivan-Lysenko <diparon@yandex.ru>
2023-11-07 16:28:03 +0300

    ADD login/logout functionality

--- public/index.php
+++ public/index.php
@@ -27,7 +27,6 @@ $app->get('/', function ($request, $response) {
 $app->get('/users', function ($request, $response) {
     $term = $request->getQueryParam('term');
     $users = json_decode($request->getCookieParam('users', json_encode([])), true);
-    //var_dump($_COOKIE);exit;
     $messages = $this->get('flash')->getMessages();
 
     $filteredUsers = array_filter($users, fn ($user) => str_contains($user['nickname'], $term));
@@ -72,12 +71,9 @@ $app->post('/users', function ($request, $response) {
         
         $newUser['id'] = uniqid();
         $existsUsers = json_decode($request->getCookieParam('users', json_encode([])), true);
-        //var_dump($newUser);
-        
         $existsUsers[] = $newUser;
         
         $encodedUsers = json_encode($existsUsers);
-        //var_dump($encodedUsers);exit;
         $this->get('flash')->addMessage('success', 'Пользователь сохранен');
 
         return $response->withHeader('Set-Cookie', "users={$encodedUsers}; Path=/")->withRedirect("/users");
@@ -90,22 +86,19 @@ $app->post('/users', function ($request, $response) {
 
 $app->get('/users/{id}/edit', function ($request, $response, $args) {
     $id = $args['id'];
-    // var_dump($id);exit;
     $users = json_decode($request->getCookieParam('users', json_encode([])), true);
-    // var_dump($id);
-    // var_dump($users);exit;
     $user = array_filter($users, fn ($user) => $user['id'] === $id);
     $user = array_values($user)[0];
-    // var_dump($user);exit;
+
     if (count($user) === 0) {
         return $response->withRedirect('users', 404);
     }
-    //var_dump($user);exit;
+
     $params = [
         'user' => $user,
         'errors' => []
     ];
-    //var_dump($params);exit;
+
     return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
 })->setName('editUser');
 
@@ -144,7 +137,6 @@ $app->patch('/users/{id}', function ($request, $response, $args) {
         'errors' => $errors,
         'user' => $user
     ];
-    //var_dump($params);exit;
 
     return $this->get('renderer')->render($response->withStatus(422), '/users/edit.phtml', $params);
 });
@@ -164,5 +156,43 @@ $app->get('/courses/{id}', function ($request, $response, $args) {
     return $response->write("Course id: {$id}");
 })->setName('course');
 
+$app->get('/login', function ($request, $response) {
+    $param = ['errors' => []];
+    return $this->get('renderer')->render($response, '/users/login.phtml', $param);
+});
+
+$app->post('/login', function ($request, $response) {
+    $email = $request->getParsedBodyParam('user')['email'];
+
+    if (!$email) {
+        $errors['email'] = 'The field is require';
+    }
+
+    $users = json_decode($request->getCookieParam('users', json_encode([])), true);
+    $user = array_filter($users, fn ($user) => $user['email'] === $email);
+    $user = array_values($user)[0];
+
+    if ($user) {
+        $this->get('flash')->addMessage('success', 'Successful authorization');
+        $_SESSION['user'] = [
+            'nickname' => $user['nickname'],
+            'email' => $user['email'],
+            'authorization' => true
+        ];
+        return $response->withRedirect("/users");
+    }
+
+    $errors['auth'] = 'User don`t exists';
+    $param = ['errors' => $errors];
+    
+    return $this->get('renderer')->render($response, '/users/login.phtml', $param);
+});
+
+$app->get('/logout', function ($request, $response) {
+    $_SESSION = [];
+    session_destroy();
+    
+    return $response->withRedirect('/users');
+});
 
 $app->run();
